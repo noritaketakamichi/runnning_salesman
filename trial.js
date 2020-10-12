9bdc337d29c8297b8fedc6cffeb08039848dfcb3
noritaketakamichi <noritaket28@gmail.com>
2020-10-13 00:11:33 +0900

    selection complete

--- trial.js
+++ trial.js
@@ -1,6 +1,7 @@
 //遺伝的アルゴリズムのトライアル
 const { generateRandomArr, addStartEnd } = require("./generateArr");
 const { calcTotalDist } = require("./calcDist");
+const { selection } = require("./selection");
 const tokyo = [0, 'tokyo', 35.68944, 139.69167]
 
 //ランダムな配列を生成
@@ -19,7 +20,12 @@ let distanceArr = [[locArr[0], calcTotalDist(locArr[0])], [locArr[1], calcTotalD
 const generationNum = 10;
 
 for (let i = 0; i < generationNum; i++) {
-    //距離が短い2ルートを抽出
-    const topTwo = pickTopTwo(distanceArr);
+    //2つのルートをルーレット選択で選択
+    const SelectedRoutes = selection(distanceArr);
+    console.log("SelectedRoutes:"+SelectedRoutes);
+
+    //一点交叉（切り離してくっつける）
+    
+
 }
 console.log(distanceArr);
